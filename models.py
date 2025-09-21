2175d9fa4aa34fbbcdc6e7ab40b133498345cf3e
matt <matt@styrofoamxylophone.com>
2025-09-21 23:02:11 +0100

    add validator in right place

--- models.py
+++ models.py
@@ -18,8 +18,16 @@ class UserRegister(BaseModel):
     
     @validator('password')
     def validate_password(cls, v):
-        if len(v) < 8:
-            raise ValueError('Password must be at least 8 characters')
+        if len(v) < 10:
+            raise ValueError('Password must be at least 12 characters')
+        if not re.search(r'[A-Z]', v):
+            raise ValueError('Password must contain at least one uppercase letter')
+        if not re.search(r'[a-z]', v):
+            raise ValueError('Password must contain at least one lowercase letter')
+        if not re.search(r'\d', v):
+            raise ValueError('Password must contain at least one number')
+        if not re.search(r'[!@#$%^&*()_+\-=\[\]{}|;:,.<>?]', v):
+            raise ValueError('Password must contain at least one special character')        
         return v
 
 class UserLogin(BaseModel):
@@ -144,21 +152,3 @@ class PostDetailResponse(PostResponse):
     edit_history: Optional[List[PostEditHistory]] = None
 
 
-@validator('password')
-def validate_password(cls, v):
-    if len(v) < 12:
-        raise ValueError('Password must be at least 12 characters')
-    
-    if not re.search(r'[A-Z]', v):
-        raise ValueError('Password must contain at least one uppercase letter')
-    
-    if not re.search(r'[a-z]', v):
-        raise ValueError('Password must contain at least one lowercase letter')
-    
-    if not re.search(r'\d', v):
-        raise ValueError('Password must contain at least one number')
-    
-    if not re.search(r'[!@#$%^&*()_+\-=\[\]{}|;:,.<>?]', v):
-        raise ValueError('Password must contain at least one special character')
-    
-    return v
